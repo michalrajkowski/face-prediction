0e2173bd4509c37fe936c12d5c28a58bd6f5cc31
michalrajkowski <infernosynek@gmail.com>
2023-09-19 00:27:24 +0200

    Cleaned the code. Changed how the output is saved: images with names as in input images but with prediction chance + image name typed on top of them. Also a text file in output is saved with all predictions as text rows

--- main.py
+++ main.py
@@ -21,37 +21,40 @@ for f in os.listdir(OUTPUT_IMG_DIR):
 CATEGORIES = ["Female", "Male"]
 IMG_SIZE = 50
 
-# Load model
-test_X = []
-test_X = np.array(test_X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+def build_model():
+  # Load model
+  test_X = []
+  test_X = np.array(test_X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
-model = Sequential()
-model.add(Conv2D(128, (4,4), input_shape = test_X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+  model = Sequential()
+  model.add(Conv2D(128, (4,4), input_shape = test_X.shape[1:]))
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(128, (4,4)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+  model.add(Conv2D(128, (4,4)))
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(128, (4,4)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+  model.add(Conv2D(128, (4,4)))
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
+  model.add(Flatten())
 
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
+  model.add(Dense(1))
+  model.add(Activation("sigmoid"))
 
-model.compile(loss="binary_crossentropy",
-              optimizer="adam",
-              metrics=['accuracy'])
+  model.compile(loss="binary_crossentropy",
+                optimizer="adam",
+                metrics=['accuracy'])
 
-model_path = './model/aug_model'
-model.load_weights(model_path)
+  model_path = './model/aug_model'
+  model.load_weights(model_path)
 
+  return model
+######
 
-def display_prediction(img, label, chance, pred_cat):
+def display_prediction(img, chance, pred_cat):
   plt.imshow(img)
   prd_gender = "Predicted: " + CATEGORIES[int(pred_cat)]
   prd_percent = "with " + str(chance) +" prob."
@@ -59,10 +62,17 @@ def display_prediction(img, label, chance, pred_cat):
   save_path = os.path.join(OUTPUT_IMG_DIR, str(time.time()) + '.png')
   plt.savefig(save_path)
 
-def load_test_images():
+def create_prediction_plot(face_image, prediction_chane, prediction_cathegory, image_name):
+  plt.imshow(face_image)
+  prd_gender_string = "Predicted: " + prediction_cathegory
+  prd_percent_string = "with " + str(prediction_chane) +"% prob."
+  plt.title( "{}\n{}\n{}".format(image_name ,prd_gender_string, prd_percent_string))
+
+def load_test_images(special_data, original_images, image_names):
     path = INPUT_IMG_DIR
     for img in os.listdir(path):
         try:
+            image_names.append(img)
             color_image = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
             RGB_img = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
             img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
@@ -72,13 +82,16 @@ def load_test_images():
             original_images.append(RGB_img)
         except Exception as e:
             print("File corrupted: " + img)
+# Create model
+model = build_model()
 
-
+# Load special data and make predictions
 SPECIAL_DATADIR = "./test_data"
 special_data = []
 original_images = []
+image_names = []
 
-load_test_images()
+load_test_images(special_data, original_images, image_names)
 
 special_X = []
 special_Y = []
@@ -93,16 +106,40 @@ special_Y = np.array(special_Y)
 special_X = special_X/255.0
 
 special_pred = model.predict(special_X, batch_size=32)
+print(special_pred)
+
+# Prepare results
+gender_chance = []
+predicted_cathegories = []
+# calculate predictions:
 
 for i in range(len(special_pred)):
-  (img, label) = special_data[i]
+  chance = special_pred[i][0]
+  predicted_gender = 0 if chance < 0.5 else 1
+  perc = 1.0 - chance if chance < 0.5 else chance
+  perc = perc*100.0
+  gender_chance.append(perc)
+  predicted_cathegories.append(CATEGORIES[predicted_gender])
+
+# Save predictions as images 
+for i in range(len(special_pred)):
+  predicted_cat = predicted_cathegories[i]
   colored_image = original_images[i]
-  chance = special_pred[i]
-  pred_gender = 0 if chance < 0.5 else 1
+  certainity_percent = gender_chance[i]
+  image_name = image_names[i]
+  print(certainity_percent, predicted_cat, image_name)
+  create_prediction_plot(colored_image, certainity_percent, predicted_cat, image_name)
+  save_path = os.path.join(OUTPUT_IMG_DIR, image_name)
+  plt.savefig(save_path)
 
-  display_prediction(colored_image, label, chance, pred_gender)
+# Save predictions as txt file
+output_txt_file = os.path.join(OUTPUT_IMG_DIR, 'predictions.txt')
 
-# How to save data?
-# Save each image with the prediction and chance
-# Save prediction to txt file as well
+with open(output_txt_file, 'w') as txt_file:
+    for i in range(len(special_pred)):
+        certainty_percent = gender_chance[i]
+        image_name = image_names[i]
+        predicted_cat = predicted_cathegories[i]
+        txt_file.write(f"{image_name} {predicted_cat} {certainty_percent}\n")
 
+txt_file.close()
